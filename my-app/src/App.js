c366c96ab0107988136c5bee9ff7eee342096d1d
ajengbellak <ajeng@fourpeople.org>
2021-05-03 20:04:13 +0700

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../src/assets/scss/bootstrap-5.0.0-beta3/scss/style.scss";
+
+function App() {
+    return ( <
+        div className = "App" >
+        Hello World <
+        /div>
+    );
+}
+
+export default App;
---
+++ my-app/src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import moduleName from '../elemens/Buttom'
+export default function landingPage() {
+    return ( <
+        div >
+
+        <
+        /div>
+    )
+}
